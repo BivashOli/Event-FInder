6e0b1131959e920b4e4444173502fa0654287faf
Bivash Oli <bivashkoli@gmail.com>
2023-12-16 18:01:21 -0500

    navbar css added

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,39 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
 import './navbar.css'
 
 const Navbar = () => {
 
-
-
      return (
           <div className="Navbar">
                <div className="flex-container">
-                    <div className="item-1">A</div>
-                    <div className="item-2">B</div>
-                    <div className="item-3">C</div>
-                    <div className="item-3">D</div>
-                    <div className="item-3">E</div>
+                    <div className="item item-1">
+                         <Link to='/feed'>
+                         🏠
+                              {/* <img src={require('../../res/navbar/icons8-home-64.png')} alt="" /> */}
+                         </Link>
+                    </div>
+                    <div className="item item-2">
+                         <Link to='/map'>
+                         🌐
+                         </Link>
+                    </div>
+                    <div className="item item-3">
+                         <Link to='/create-event'>
+                         ➕
+                         </Link>
+                    </div>
+                    <div className="item item-4">
+                         <Link to='/notifications'>
+                         ❤️
+                         </Link>
+                    </div>
+                    <div className="item item-5">
+                         <Link to='/my-profile'>
+                         😺
+                         </Link>
+                    </div>
                </div>
 
           </div>
