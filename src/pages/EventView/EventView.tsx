05a75ade4df08c452c28adceba6fd413e261d32a
Bivash Oli <bivashkoli@gmail.com>
2023-12-20 21:01:40 -0500

    responsive buttons in EventView

--- src/pages/EventView/EventView.tsx
+++ src/pages/EventView/EventView.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import UserCard from '../../components/UserCard';
 import Navbar from '../../components/Navbar';
 import { Link, useLocation } from 'react-router-dom';
@@ -10,21 +10,33 @@ const EventView = (props) => {
 
      const location = useLocation()
      const userUsername = location.state ? location.state.userUsername : null;
+     const [isFollowing, setFollowing] = useState(false) // initial value should be retrieved from database not just false
+     const [isAttending, setAttending] = useState(false) // initial value should be retrieved from database not just false
 
-          
+
      return (
           <div className="EventView">
                <div className="top-section-container">
                     <div className="top-section-item user-info">
                          <UserCard userProfilePic="" username={userUsername} />
-                         <Link to="" className="follow-button">Follow</Link>
+                         {(isFollowing) ? (<button onClick={() => setFollowing(false)} className="unfollow-button">Unfollow</button>
+                         ) : (<button onClick={() => setFollowing(true)} className="follow-button">Follow</button>
+                         )}
 
                     </div>
                     <div className="top-section-item event-info">
                          <h1>🧘🏾Morning stretches meetup!</h1>
                          <h4>Dec 17th 9 am</h4>
                          <p>xyz people are attending</p>
-                         <Link to="" className="attend-button">Attend</Link>
+
+
+                         {
+                         (isAttending) ? (<button onClick={() => setAttending(false)} className="unattend-button">Unattend</button>
+                         ) : (<button onClick={() => setAttending(true)} className="attend-button">Attend</button>
+                         )
+                         }
+
+                         {/* <Link to="" className="attend-button">Attend</Link> */}
                          <p>Description</p>
                     </div>
                </div>
