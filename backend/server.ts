7f4a1d1b9c79d3b28bee43c5d0d3c634dc26c592
Bivash Oli <bivashkoli@gmail.com>
2023-12-22 14:12:20 -0500

    adding mongoose for mongodb

--- backend/server.ts
+++ backend/server.ts
@@ -1,3 +1,17 @@
+
 const express = require('express')
+const mongoose = require('mongoose')
+
+require('dotenv').config({path : '.env.local'})
 
 const app = express()
+
+const dbURI = process.env.DB_URI
+console.log(dbURI)
+mongoose.connect(dbURI, {useNewUrlParser : true, useUnifiedTopology : true}).then(
+     (result : any) => {
+          console.log('connected to db')
+     }
+).catch((err : any) => console.log(err))
+
+app.listen(3001) 
