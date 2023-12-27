d35ba7c338be283d60c7648d336240987e76b804
Bivash Oli <bivashkoli@gmail.com>
2023-12-27 14:10:25 -0500

    /login route successfully adds new user in db if it doesnt exist

--- backend/routes/UserRoutes.ts
+++ backend/routes/UserRoutes.ts
@@ -1,23 +1,52 @@
-import express, {Request, Response} from 'express'
+import express, { Request, Response } from 'express'
 import { Express } from 'express'
-import jwt, { JwtPayload } from 'jsonwebtoken'
+import { LoginTicket, OAuth2Client } from 'google-auth-library'
+import dotenv from 'dotenv'
+import User from '../models/User'
 
+dotenv.config({ path: '.env.local' })
 const router: express.Router = express.Router()
 
-router.post('/login', (req : Request, res : Response) => {
-     const {credential} = req.body
-     const decodedToken : string | jwt.JwtPayload | null = jwt.decode(credential)
+router.post('/login', (req: Request, res: Response) => {
+     const { credential } = req.body
 
-     if (typeof decodedToken != 'string' && decodedToken != null){
+     const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID)
 
-          const userEmail = decodedToken?.email;
-     }
-     // Check if this userEmail exists already
-     // If it doesnt exist, create them as a new user in DB
-     // Regardless of the if statement, redirect them to map page
+     let email: string | undefined
+     let sub: string | undefined
+     client.verifyIdToken({ idToken: credential, audience: process.env.GOOGLE_CLIENT_ID })
+          .then((ticket: LoginTicket) => {
+               const payload = ticket.getPayload()
+               if (payload) {
+                    email = payload.email;
+                    sub = payload.sub
+                    console.log(email)
+               } else {
+                    console.log("ERROR: No payload found")
+               }
+          }).then(() => {
+
+               User.findOne({ email: email }).then((user) => {
+                    if (!user) {
+                         const newUser = new User({
+                              username: "testinggg",
+                              sub: sub,
+                              email: email,
+                              dob: new Date(),
+                              public: true
+                         }).save().then((result) => {
+                              console.log(result)
+
+                         }).catch(err => console.log(err))
+                    }else{
+                         console.log("USER ALREADY EXISTS")
+                    }
+
+               })
+
+          })
+
+     res.json({ success: 1 })
 })
-
-// router.delete() 
-
 
 export default router
